a5c57593d8617678f2ef6ce7466ed2f7dbd68409
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2022-05-20 16:43:54 +0200

    Fix url connexion

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,8 +1,8 @@
-import config from "config";
+import config from 'config';
 
 // const { host, port, database }: dbConfig = config.get('dbConfig');
 const { DATABASE_USER, DATABASE_PASSWORD } = process.env;
 
 export const dbConnection: { url: string } = {
-  url: `mongodb://127.0.0.1:27017/pokedex`,
+  url: `mongodb://localhost:27017/pokedex?readPreference=primary&directConnection=true&ssl=false/`,
 };
